4f75c1d1666375a8e7bfd2dbd38bb4ff4f5bce77
Miles Padot <mpadot@emich.edu>
2024-11-06 14:51:45 -0500

    Creation

---
+++ src/stack.java
@@ -0,0 +1,38 @@
+public class stack {
+    Node head;
+    Node tail;
+    public class Node{
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if(head ==null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void pop(){
+        if(head == null){
+            System.out.println("Stack is empty");
+        }
+        else{
+            head = head.next;
+        }
+    }
+    public void printList(){
+        Node current = head;
+        while(current.next != null){
+            System.out.print(current.data+" ");
+        }
+    }
+
+}
